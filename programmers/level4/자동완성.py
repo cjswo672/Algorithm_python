9ab077593dd3792b1359c1dac420a9b0bd1b3d2c
cjswo672@naver.com <cjswo672@naver.com>
2020-04-08 09:23:04 +0900

    [Trie] Trie보다 정렬 후 비교가 훨씬 빠름

--- programmers/level4/자동완성.py
+++ programmers/level4/자동완성.py
@@ -5,6 +5,22 @@ class Trie:
 
 
 def solution(words):
+    words.sort()
+    answer = min(equal_length(words[0], words[1]) + 1, len(words[0]))
+    for i in range(1, len(words) - 1):
+        prev, next = words[i - 1], words[i + 1]
+        answer += min(max(equal_length(prev, words[i]), equal_length(next, words[i])) + 1, len(words[i]))
+    answer += min(equal_length(words[-2], words[-1]) + 1, len(words[-1]))
+    return answer
+
+
+def equal_length(word1, word2):
+    for i, (ch1, ch2) in enumerate(zip(word1, word2)):
+        if ch1 != ch2: return i
+    return min(len(word1), len(word2))
+
+
+def solution2(words):
     root = Trie()
     for word in words:
         get_tries(root, word)
