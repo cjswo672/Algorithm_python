6a98827da348bd910b395562392e6a5d54935129
cjswo672@naver.com <cjswo672@naver.com>
2020-04-26 08:54:34 +0900

    [Parametric Search] 효율성 2개 시간초과, 정확성 2개 틀림

--- programmers/level4/지형편집.py
+++ programmers/level4/지형편집.py
@@ -1,7 +1,28 @@
-# 못품
-def solution(land, P, Q):
-    answer = -1
-    return answer
+# 효율성 3개 시간초과, 정확성 2개 틀림
+def solution(lands, P, Q):
+    lands = sum(lands, [])
+    l, r, v = 0, max(lands), 0
+    while l <= r:
+        m, v = (l + r) >> 1, 0
+        a, b, c = 0, 0, 0  # m + 1 층, m 층, m - 1층
+        for land in lands:
+            if land < m:
+                v = v + (m - land) * P
+                c += 1
+            elif land > m:
+                v = v + (land - m) * Q
+                a += 1
+            else: b += 1
+
+        p = v - a * Q + (b + c) * P     # m + 1층: a * - Q + (b + c) * P -> len(lends) - a * P
+        pp = v - c * P + (a + b) * Q    # m - 1층: c * - P + (a + b) * Q
+
+        print(f'{m + 1}층: {p}, {m}층: {v}, {m - 1}층: {pp}, ({a}, {b}, {c})')
+
+        if p > v < pp: return v
+        elif p < v < pp: l = m + 1
+        else: r = m - 1
+    return v
 
 
 # 막 풀기
@@ -29,4 +50,8 @@ def calculate(land, P, Q, v):
     return res
 
 
+print(solution([[4, 4, 3], [3, 2, 2], [ 2, 1, 0 ]], 5, 3))
 print(solution1([[4, 4, 3], [3, 2, 2], [ 2, 1, 0 ]], 5, 3))
+print("-------------")
+print(solution([[1, 2], [2, 3]], 3, 2))
+print(solution1([[1, 2], [2, 3]], 3, 2))
